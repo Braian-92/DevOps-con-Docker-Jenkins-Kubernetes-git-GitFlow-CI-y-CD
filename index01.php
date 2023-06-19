5d19467ee615fb3ca6c216b56ab86cb9fbf4d56d
Braian-92 <zamudiobraianhernan@gmail.com>
2023-06-19 18:48:33 -0300

    base devops

---
+++ index01.php
@@ -0,0 +1,36 @@
+Que es devops
+DEVELOPEMENT OPERATION
+Union de personas, procesos y tecnologia con el fin valor continuamente a los clientes
+Entregar mejores productos en menor tiempo y con mucha mas calidad
+
+####### Aspetos fundamentales de DevOps ##########
+
+Control de versiones: git, svn -> Github, gitlab , bitbucket, mercurial
+
+Integración continua: Pipelines jenkins, Automatizar compilaciones y pruebas cuando se hace el commit
+
+Entrega continua: Suministro de software rapido y confiable en cualquier momento
+
+Infraestructura como código: Terraform -> Definición declarativa de la infraestructura mediante archivos de definición basados
+en texto para evitar revertir, desmontar y recrear entornos complejos
+
+Supervición y registro: Prometheus, grafana -> Monitorización, recompilación de métricas y vinculación de datos de performance. 
+
+Apendisaje validado: Analisis de datos para mejorar los procesos en cada ciclo nuevo
+
+
+####### Site reliability Enginer (devops)######
+
+############# OPERACIONES #############
+
+Docker => crear contenedores, crear imagenes y hacer orquestaciones (empaquetar aplicaciones)
+Kubernetes => (orquestar contenedores) e infraestructura como código (ejecutar contenedores)
+Prometheus / Grafana => Observabilidad (monitorización)
+
+
+############# DESARROLLO #############
+
+Git-flow
+Github-GitLab
+Microservicios (agilidad en contenedores)
+Pruebas unitarias
